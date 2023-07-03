a8a41cd86220d3c5ddeefc7ac64467977e4200ba
VladimirSazanakov <sazanakov@mail.ru>
2023-07-04 00:55:38 +0700

    1.3.5

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const TodoList = () =>{
+  const items = ['learn React', 'Build Awesome App', 'Drink coffe']
   return (
     <ul>
-    <li>Learn React</li>
-    <li>Build Awesome App</li>
+    <li>{items[0]} </li>
+    <li>{items[1]}</li>
+    <li>{items[2]}</li>
   </ul>
   );
 };
@@ -17,14 +19,24 @@ const AppHeader = () => {
 }
 
 const SearchPanel = () => {
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '25px'
+  };
   return (
-    <input placeholder='search'></input>
+    <input 
+    style = {searchStyle} placeholder={searchText} ></input>
   )
 }
 
 const App = () => {
+  const isLoggedIn = true;
+  const loginBox = <span>Log in please</span>
+  const WelcomeBox = <span>Welcome Back</span>
   return (
     <div>
+      <span>{(new Date().toString())} </span>
+      {isLoggedIn ? WelcomeBox : loginBox}
       <AppHeader/>
       <SearchPanel/>
       <TodoList/>
