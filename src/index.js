a1b8bfb708334a00a83ef473205ccd3ab1fb4ba4
VladimirSazanakov <sazanakov@mail.ru>
2023-07-03 23:54:02 +0700

    1.3.3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello React');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = (
+  <div>
+  <h1>My Todo List</h1>
+  <input placeholder='search'></input>
+  <ul>
+    <li>Learn React</li>
+    <li>Build Awesome App</li>
+  </ul>
+  </div>
+);
+
+console.log(el);
+
+ReactDom.render(el, document.getElementById('root'));
