d96007b374539c318629acf75ae5e2c3c112c50c
VladimirSazanakov <sazanakov@inbox.ru>
2023-07-05 23:33:08 +0700

    1.4.7

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
 import AppHeader from '../app-header';
@@ -8,32 +8,48 @@ import TodoList from '../todo-list';
 import './app.css';
 import ItemStatusFilter from '../item-status-filter';
 
-const App = () => {
-  const todoData = [
+export default class App extends Component {
+  constructor(){
+    super()
+  };
+  
+  state ={ 
+    todoData: [
     { label: 'Drink Coffe', important: false, id: 1 },
     { label: 'Make Awesome App', important: true, id: 2 },
     { label: 'Have a lunch', important: false, id: 3 }
   ]
+};
 
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>
-  const WelcomeBox = <span>Welcome Back</span>
+deleteItem = (id) => {
+  console.log(id);
+  this.setState(({todoData})=>{
+    const idx = todoData.findIndex((el) => el.id == id);
+    console.log(idx);
+    //todoData.splice(idx, 1);
+    const before = todoData.slice(0, idx);
+    const after = todoData.slice(idx +1);
+    const newArray = [...before, ...after];
+
+    return {todoData: newArray};
+  })
+}
+
+render(){
+  
   return (
     <div className="todo-app">
-      <span>{(new Date().toString())} </span>
-      {isLoggedIn ? WelcomeBox : loginBox}
-      <AppHeader />
+      <AppHeader toDo={1} done={3} />
       <div className="top-panel d-flex">
         <SearchPanel />
         <ItemStatusFilter />
       </div>
-      <TodoList todos={todoData}
-        onDeleted={(id) => {
-          console.log('del', id);
-        }} />
+      <TodoList todos={this.state.todoData}
+        onDeleted={this.deleteItem} />
 
     </div>
   )
+      }
 }
 
-export default App;
+//export default App;
