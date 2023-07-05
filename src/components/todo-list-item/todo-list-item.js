2fd40222ec78b4e55a3c1ed44f0e9f2541d56b80
VladimirSazanakov <sazanakov@inbox.ru>
2023-07-05 15:13:50 +0700

    1.4.2

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,24 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-  render () {
+
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
+  }
+
+
+  render() {
     const { label, important = false } = this.props;
+
     const style = {
-      color: important ? 'tomato' : 'black'
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
     };
-  
+
     return (
       <span className="todo-list-item">
         <span className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick}>
           {label}
         </span>
         <button type="button"
           className="btn btn-outline-success btn-sm float-rigth">
           <i className="fa fa-exclamation" />
         </button>
-  
+
         <button type="button"
           className="btn btn-outline-danger btn-sm float-rigth">
           <i className="fa fa-trash-o" />
