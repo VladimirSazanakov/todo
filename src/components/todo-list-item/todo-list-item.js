f0fe731653bd2a855b02da7a0b2d95813256701e
VladimirSazanakov <sazanakov@inbox.ru>
2023-07-06 10:30:45 +0700

    dont now

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -55,7 +55,7 @@ export default class TodoListItem extends Component {
         <button type="button"
           className="btn btn-outline-danger btn-sm float-right"
           onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
+          <i className="fa-solid fa-trash-can" />
         </button>
       </span>
     );
